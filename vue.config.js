9a1cdcc6f3de5d9064d60dd05ef26ccd65c208f4
Denis Sales <denisales@hotmail.com>
2020-08-02 13:02:18 -0300

    adicionando imagens de icones no pwa e favicon

--- vue.config.js
+++ vue.config.js
@@ -10,14 +10,65 @@ module.exports = {
     },
   },
   pwa: {
-    name: 'Netshoes Cart',
+    name: 'Shop Cart',
     themeColor: '#000000',
     msTileColor: '#000000',
+    iconPaths: {
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
     // configure the manifest options
     manifestOptions: {
       display: 'standalone',
       background_color: '#000000',
       orientation: 'portrait',
+      icons: [
+        {
+          src: 'img/icons/android-chrome-72x72.png',
+          sizes: '72x72',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-96x96.png',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-192x192-maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: 'img/icons/android-chrome-384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/android-chrome-512x512-maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
     },
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
